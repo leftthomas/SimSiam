c709e90e0ab51bb7c353a84f7d0eac2fd624e081
leftthomas <leftthomas@qq.com>
2020-12-23 16:52:34 +0800

    make the default scale to 20

--- utils.py
+++ utils.py
@@ -91,7 +91,7 @@ def recall(feature_vectors, feature_labels, rank):
 
 
 class NormalizedSoftmaxLoss(nn.Module):
-    def __init__(self, scale=32):
+    def __init__(self, scale=20):
         super(NormalizedSoftmaxLoss, self).__init__()
         self.scale = scale
 
