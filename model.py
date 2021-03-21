730753ba6d5368e3a884d03bd54a3af7b8c3b061
leftthomas <leftthomas@qq.com>
2021-03-21 22:54:53 +0800

    finish model

--- model.py
+++ model.py
@@ -1,15 +1,14 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torchvision.models import resnet18
 
 
 class Model(nn.Module):
-    def __init__(self, proj_dim=128):
+    def __init__(self, feature_dim=2048):
         super(Model, self).__init__()
 
         self.f = []
-        for name, module in resnet18().named_children():
+        for name, module in resnet18(zero_init_residual=True).named_children():
             if name == 'conv1':
                 module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
             if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
@@ -17,11 +16,15 @@ class Model(nn.Module):
         # encoder
         self.f = nn.Sequential(*self.f)
         # projection head
-        self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
-                               nn.ReLU(inplace=True), nn.Linear(512, proj_dim, bias=True))
+        self.g = nn.Sequential(nn.Linear(512, 2048, bias=False), nn.BatchNorm1d(2048),
+                               nn.ReLU(inplace=True), nn.Linear(2048, feature_dim, bias=False),
+                               nn.BatchNorm1d(feature_dim))
+        # prediction head
+        self.h = nn.Sequential(nn.Linear(feature_dim, feature_dim // 4, bias=False), nn.BatchNorm1d(feature_dim // 4),
+                               nn.ReLU(inplace=True), nn.Linear(feature_dim // 4, feature_dim, bias=True))
 
     def forward(self, x):
-        x = self.f(x)
-        feature = torch.flatten(x, start_dim=1)
-        out = self.g(feature)
-        return F.normalize(feature, dim=-1), F.normalize(out, dim=-1)
+        x = torch.flatten(self.f(x), start_dim=1)
+        feature = self.g(x)
+        proj = self.h(feature)
+        return feature, proj
